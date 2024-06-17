9cc5e574a96d16ecf6f42b6c226ecbc9ed115855
Ayush1234567434324 <ayushbhandari3031@gmail.com>
2024-06-17 21:15:31 +0530

    op

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client"; // import connection function
 
-const socket = io('192.168.1.103:8080'); // initialize websocket connection
+const socket = io('192.168.1.104:8080'); // initialize websocket connection
 
 export default socket;
